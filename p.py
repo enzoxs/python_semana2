48da0d4605eb633504e8aec21f62b235aaea6add
Emily <emily.kirimix@gmail.com>
2023-08-27 16:02:45 -0400

    first commit

---
+++ p.py
@@ -0,0 +1,24 @@
+v_total = 0
+v_diaria = 0
+index = 0
+
+
+nombre_cliente = input("Ingrese el nombre del cliente:")
+rut = input("Ingrese rut de cliente")
+kilos= input("Ingrese la totalidad de kilos")
+
+
+
+print (f"El nombre del cliente es : {self.nombre_cliente}")
+print(f"El rut del ususario es : {self.rut}")
+print(f"La totalidad de kilos a comprar es de : {self.kilos}")
+
+
+
+
+
+
+
+
+
+
